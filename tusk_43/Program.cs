8d2bac11fe6f5485b8ef49e6c1f4a2e92b2257c8
Artyom <tatubalin.artem@mail.ru>
2023-01-23 23:56:36 +0800

    tusk_43

---
+++ tusk_43/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Clear();
+
+double getUserCoefficient(string massege)
+{
+    Console.WriteLine(massege);
+    double coef = double.Parse(Console.ReadLine()!);
+    return coef;
+}
+
+double Intersection(double b1, double k1, double b2, double k2)
+{
+    double result = 0;
+    if (k1 == k2)
+    {
+        Console.WriteLine("Точек пересечения нет.");
+    }
+    else
+    {
+        result = (b2 - b1) / (k1 - k2);
+    }
+    return result;
+}
+
+double b1 = getUserCoefficient("Введите b1: ");
+double k1 = getUserCoefficient("Введите k1: ");
+double b2 = getUserCoefficient("Введите b2: ");
+double k2 = getUserCoefficient("Введите k2: ");
+
+double res = Intersection(b1, k1, b2, k2);
+Console.WriteLine($"Точка пересечения функций х = {res}");
